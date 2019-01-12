37f06465519dda04e26d85f21d45f293d26e1e23
nqd <dinh.nguyen@veriksystems.com>
2019-01-12 17:21:00 +0700

    check the prefix

--- flat.go
+++ flat.go
@@ -18,14 +18,15 @@ func flatten(prefix string, nested interface{}, opts Options) (m map[string]inte
 	switch nested.(type) {
 	case map[string]interface{}:
 		// map
-		log.Println("flatten a map")
 		for k, v := range nested.(map[string]interface{}) {
-			newKey := prefix + "." + k
+			newKey := k
+			if prefix != "" {
+				newKey = prefix + "." + k
+			}
 			switch v.(type) {
 			case map[string]interface{}, []interface{}:
 				m, err = flatten(newKey, v, opts)
 			default:
-				log.Println("default")
 				m = make(map[string]interface{})
 				m[newKey] = v
 			}
