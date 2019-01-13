c762e8cfd270e06d8882ca0f3805d44953e21567
nqd <dinh.nguyen@veriksystems.com>
2019-01-13 12:06:50 +0700

    flat slice

--- flat.go
+++ flat.go
@@ -3,6 +3,7 @@ package flat
 import (
 	"log"
 	"reflect"
+	"strconv"
 )
 
 type Options struct {
@@ -25,6 +26,7 @@ func flatten(prefix string, nested interface{}, opts Options) (m map[string]inte
 	m = make(map[string]interface{})
 	switch nested := nested.(type) {
 	case map[string]interface{}:
+		log.Println("map", nested)
 		// map
 		for k, v := range nested {
 			// create new key
@@ -53,8 +55,33 @@ func flatten(prefix string, nested interface{}, opts Options) (m map[string]inte
 			}
 		}
 	case []interface{}:
-		log.Println("slice")
+		log.Println("slice", nested)
 		// slice
+		for i, v := range nested {
+			newKey := strconv.Itoa(i)
+			if prefix != "" {
+				newKey = prefix + opts.Delimiter + newKey
+			}
+			switch v.(type) {
+			case map[string]interface{}, []interface{}:
+				temp, fErr := flatten(newKey, v, opts)
+				if fErr != nil {
+					err = fErr
+					return
+				}
+				// empty map {}
+				if reflect.DeepEqual(temp, map[string]interface{}{}) {
+					m[newKey] = temp
+				} else {
+					// merge temp to m
+					for kt, vt := range temp {
+						m[kt] = vt
+					}
+				}
+			default:
+				m[newKey] = v
+			}
+		}
 	default:
 		log.Println("error")
 	}
