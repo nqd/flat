1d786836c14836efc237efbeb25cae9d054be34b
nqd <nqdinhddt@gmail.com>
2019-01-20 14:51:23 +0700

    adding another tests for Flatten

--- flat_test.go
+++ flat_test.go
@@ -13,7 +13,7 @@ func TestFlatten(t *testing.T) {
 		options Options
 	}{
 		// test with different primitives
-		// String: 'good morning',
+		// String: 'world',
 		// Number: 1234.99,
 		// Boolean: true,
 		// null: null,
@@ -37,6 +37,12 @@ func TestFlatten(t *testing.T) {
 			map[string]interface{}{"hello": nil},
 			Options{},
 		},
+		// nested once
+		{
+			`{"hello":{}}`,
+			map[string]interface{}{"hello": map[string]interface{}{}},
+			Options{},
+		},
 		{
 			`{"hello":{"world":"good morning"}}`,
 			map[string]interface{}{"hello.world": "good morning"},
@@ -57,131 +63,95 @@ func TestFlatten(t *testing.T) {
 			map[string]interface{}{"hello.world": nil},
 			Options{},
 		},
-		// 		// slice
-		// 		{
-		// 			`{"hello":{"world":["one","two"]}}`,
-		// 			map[string]interface{}{
-		// 				"hello.world.0": "one",
-		// 				"hello.world.1": "two",
-		// 			},
-		// 			Options{},
-		// 		},
-		// 		// nested twice
-		// 		{
-		// 			`{"hello":{"world":{"again":"good morning"}}}`,
-		// 			map[string]interface{}{"hello.world.again": "good morning"},
-		// 			Options{},
-		// 		},
-		// 		// multiple keys
-		// 		{
-		// 			`{
-		// 				"hello": {
-		// 					"lorem": {
-		// 						"ipsum":"again",
-		// 						"dolor":"sit"
-		// 					}
-		// 				},
-		// 				"world": {
-		// 					"lorem": {
-		// 						"ipsum":"again",
-		// 						"dolor":"sit"
-		// 					}
-		// 				}
-		// 			}`,
-		// 			map[string]interface{}{
-		// 				"hello.lorem.ipsum": "again",
-		// 				"hello.lorem.dolor": "sit",
-		// 				"world.lorem.ipsum": "again",
-		// 				"world.lorem.dolor": "sit"},
-		// 			Options{},
-		// 		},
-		// 		// empty object
-		// 		{
-		// 			`{"hello":{"empty":{"nested":{}}}}`,
-		// 			map[string]interface{}{"hello.empty.nested": map[string]interface{}{}},
-		// 			Options{},
-		// 		},
-		// 		// custom delimiter
-		// 		{
-		// 			`{"hello":{"world":{"again":"good morning"}}}`,
-		// 			map[string]interface{}{"hello:world:again": "good morning"},
-		// 			Options{Delimiter: ":"},
-		// 		},
-		// 		// custom depth
-		// 		{
-		// 			`{
-		// 				"hello": {
-		// 					"world": {
-		// 						"again": "good morning"
-		// 					}
-		// 				},
-		// 				"lorem": {
-		// 					"ipsum": {
-		// 						"dolor": "good evening"
-		// 					}
-		// 				}
-		// 			}
-		// 			`,
-		// 			map[string]interface{}{
-		// 				"hello.world": map[string]interface{}{"again": "good morning"},
-		// 				"lorem.ipsum": map[string]interface{}{"dolor": "good evening"},
-		// 			},
-		// 			Options{MaxDepth: 2},
-		// 		},
+		// slice
+		{
+			`{"hello":{"world":["one","two"]}}`,
+			map[string]interface{}{
+				"hello.world.0": "one",
+				"hello.world.1": "two",
+			},
+			Options{},
+		},
+		// nested twice
+		{
+			`{"hello":{"world":{"again":"good morning"}}}`,
+			map[string]interface{}{"hello.world.again": "good morning"},
+			Options{},
+		},
+		// multiple keys
+		{
+			`{
+				"hello": {
+					"lorem": {
+						"ipsum":"again",
+						"dolor":"sit"
+					}
+				},
+				"world": {
+					"lorem": {
+						"ipsum":"again",
+						"dolor":"sit"
+					}
+				}
+			}`,
+			map[string]interface{}{
+				"hello.lorem.ipsum": "again",
+				"hello.lorem.dolor": "sit",
+				"world.lorem.ipsum": "again",
+				"world.lorem.dolor": "sit"},
+			Options{},
+		},
+		// empty object
+		{
+			`{"hello":{"empty":{"nested":{}}}}`,
+			map[string]interface{}{"hello.empty.nested": map[string]interface{}{}},
+			Options{},
+		},
+		// custom delimiter
+		{
+			`{"hello":{"world":{"again":"good morning"}}}`,
+			map[string]interface{}{"hello:world:again": "good morning"},
+			Options{Delimiter: ":"},
+		},
+		// custom depth
+		{
+			`{
+				"hello": {
+					"world": {
+						"again": "good morning"
+					}
+				},
+				"lorem": {
+					"ipsum": {
+						"dolor": "good evening"
+					}
+				}
+			}
+			`,
+			map[string]interface{}{
+				"hello.world": map[string]interface{}{"again": "good morning"},
+				"lorem.ipsum": map[string]interface{}{"dolor": "good evening"},
+			},
+			Options{MaxDepth: 2},
+		},
 
-		// 		// should parse array when safe = true
-		// 		{
-		// 			`{"hello":{"world":["one","two"]}}`,
-		// 			map[string]interface{}{
-		// 				"hello.world": []interface{}{"one", "two"},
-		// 			},
-		// 			Options{Safe: true},
-		// 		},
-		// 		// todo: why this does not work
-		// 		// {
-		// 		// 	`
-		// 		// 	{
-		// 		// 		"hello": [{
-		// 		// 			"world": {
-		// 		// 				"again": "foo"
-		// 		// 			}
-		// 		// 		}, {
-		// 		// 			"lorem": "ipsum"
-		// 		// 		}],
-		// 		// 		"another": {
-		// 		// 			"nested": [{
-		// 		// 				"array": {
-		// 		// 					"too": "deep"
-		// 		// 				}
-		// 		// 			}]
-		// 		// 		},
-		// 		// 		"lorem": {
-		// 		// 			"ipsum": "whoop"
-		// 		// 		}
-		// 		// 	}
-		// 		// 	`,
-		// 		// 	map[string]interface{}{
-		// 		// 		"hello": []map[string]interface{}{
-		// 		// 			{
-		// 		// 				"world": map[string]interface{}{
-		// 		// 					"again": "foo",
-		// 		// 				},
-		// 		// 			},
-		// 		// 			{
-		// 		// 				"lorem": "ipsum",
-		// 		// 			},
-		// 		// 		},
-		// 		// 		"another.nested": []map[string]interface{}{
-		// 		// 			{
-		// 		// 				"array": map[string]interface{}{
-		// 		// 					"too": "deep",
-		// 		// 				},
-		// 		// 			},
-		// 		// 		},
-		// 		// 		"lorem.ipsum": "whoop",
-		// 		// 	},
-		// 		// 	Options{Safe: true},
-		// 		// },
+		// slice, by default safe = false
+		{
+			`{"hello":{"world":["one","two"]}}`,
+			map[string]interface{}{
+				"hello.world.0": "one",
+				"hello.world.1": "two",
+			},
+			Options{},
+		},
+		// should parse array when safe = true
+		{
+			`{"hello":{"world":["one","two"]}}`,
+			map[string]interface{}{
+				"hello.world": []interface{}{"one", "two"},
+			},
+			Options{Safe: true},
+		},
 	}
 	for i, test := range tests {
 		var given interface{}
