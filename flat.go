3315462948d36b00840765cfe88c67e7d2e0a5c4
LennyFino <lb@fino.digital>
2019-10-24 15:02:01 +0200

    remove all flatten logic

--- flat.go
+++ flat.go
@@ -1,8 +1,6 @@
 package flat
 
 import (
-	"reflect"
-	"strconv"
 	"strings"
 
 	"github.com/imdario/mergo"
@@ -14,75 +12,6 @@ type Options struct {
 	Delimiter string
 	Safe      bool
 	MaxDepth  int
-}
-
-// Flatten the map, it returns a map one level deep
-// regardless of how nested the original map was.
-// By default, the flatten has Delimiter = ".", and
-// no limitation of MaxDepth
-func Flatten(nested map[string]interface{}, opts *Options) (m map[string]interface{}, err error) {
-	if opts == nil {
-		opts = &Options{
-			Delimiter: ".",
-		}
-	}
-
-	m, err = flatten("", 0, nested, opts)
-
-	return
-}
-
-func flatten(prefix string, depth int, nested interface{}, opts *Options) (flatmap map[string]interface{}, err error) {
-	flatmap = make(map[string]interface{})
-
-	switch nested := nested.(type) {
-	case map[string]interface{}:
-		if opts.MaxDepth != 0 && depth >= opts.MaxDepth {
-			flatmap[prefix] = nested
-			return
-		}
-		if reflect.DeepEqual(nested, map[string]interface{}{}) {
-			flatmap[prefix] = nested
-			return
-		}
-		for k, v := range nested {
-			// create new key
-			newKey := k
-			if prefix != "" {
-				newKey = prefix + opts.Delimiter + newKey
-			}
-			fm1, fe := flatten(newKey, depth+1, v, opts)
-			if fe != nil {
-				err = fe
-				return
-			}
-			update(flatmap, fm1)
-		}
-	case []interface{}:
-		if opts.Safe {
-			flatmap[prefix] = nested
-			return
-		}
-		if reflect.DeepEqual(nested, []interface{}{}) {
-			flatmap[prefix] = nested
-			return
-		}
-		for i, v := range nested {
-			newKey := strconv.Itoa(i)
-			if prefix != "" {
-				newKey = prefix + opts.Delimiter + newKey
-			}
-			fm1, fe := flatten(newKey, depth+1, v, opts)
-			if fe != nil {
-				err = fe
-				return
-			}
-			update(flatmap, fm1)
-		}
-	default:
-		flatmap[prefix] = nested
-	}
-	return
 }
 
 // update is the function that update to map with from
